3abbf605e42b329736647e6e64845d8f7786a228
809624748@qq.com <809624748@qq.com>
2019-12-18 12:55:04 +0800

    js

--- PDF_test/1.3/js/main.js
+++ PDF_test/1.3/js/main.js
@@ -1,13 +1,35 @@
+/*
 var app =new Vue({
-    el:'#app',
+    el:'#app1',
     data: {
         message:'',
         picked:'js',
+        checked:['js','html']
     },
-    /*method:{
+    /!*method:{
       handleInput:function (e) {
           this.message=e.target.value;
 
       }
-    }*/
-})
+    }*!/
+})*/
+
+var app2=new Vue({
+        el:"#app2",
+        data:{
+            selected:'html',
+            options:[{
+                text:'HTML',
+                value:'html',
+            },
+                {
+                    text:'JavaScript',
+                    value:'js'
+                },
+                {
+                    text:'CSS',
+                    value:'css'
+                },
+            ]
+        }
+    })
